ff64bac040c7939df55dbebd48e4781d6b91c9b0
Mukesh <mukesh@Luckys-MacBook-Pro.local>
2020-12-17 11:41:56 +0530

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :cabs do 
+    member do
+      post :book
+      post :end_trip
+      post :update_city
+    end
+
+    collection do 
+      post :bulk_onboard
+    end
+  end
 end
